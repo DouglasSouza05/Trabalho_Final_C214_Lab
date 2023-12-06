cdc067a549383f93f3672ab3329c684aa4e1ca2e
DouglasSouza05 <douglasbsouza01@gmail.com>
2023-12-06 20:33:47 -0300

    Começando criação da classe gerenciador

---
+++ ProjetoAgenda/Agenda/gerenciador.py
@@ -0,0 +1,21 @@
+from .contatos import Contato
+import json
+
+class Gerenciador:
+    def __init__(self):
+        self.contatos = []
+
+    def add_contato(self, contato):
+        pass
+
+    def all_contatos(self):
+        pass
+
+    def search_contato(self, nome):
+        pass
+
+    def remove_contato(self, nome):
+        pass
+
+    def save_contatos(self):
+        pass
