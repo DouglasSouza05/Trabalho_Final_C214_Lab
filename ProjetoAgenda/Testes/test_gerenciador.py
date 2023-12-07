4bf6791e9f216325a58dabbc0918c130cffac1d2
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-12-07 17:05:30 -0300

    nao lemnro o q eu fiz

--- ProjetoAgenda/Testes/test_gerenciador.py
+++ ProjetoAgenda/Testes/test_gerenciador.py
@@ -5,27 +5,22 @@ import json
 from unittest.mock import patch, MagicMock
 from ProjetoAgenda.Agenda.gerenciador import Gerenciador, Config
 
-def test_add_contato():
-    gerenciador = Gerenciador()
+@pytest.fixture
+def gerenciador_com_contatos(tmp_path):
+    gerenciador = Gerenciador(config=Config(contatos_file=tmp_path / "contatos.json"))
     gerenciador.add_contato(nome="João", sobrenome="Silva", telefone="123456789", empresa="ABC LTDA", email="joao.silva@example.com")
-    assert len(gerenciador.contatos) == 1
+    return gerenciador
 
-def test_list_contatos(capsys):
-    gerenciador = Gerenciador()
-    gerenciador.add_contato(nome="João", sobrenome="Silva", telefone="123456789", empresa="ABC LTDA", email="joao.silva@example.com")
-    
-    gerenciador.list_contatos()
+def test_list_contatos(capsys, gerenciador_com_contatos):
+    gerenciador_com_contatos.list_contatos()
     captured = capsys.readouterr()
     assert "João Silva" in captured.out
 
 @patch("builtins.open", new_callable=MagicMock)
-def test_save_contatos(mock_open):
-    gerenciador = Gerenciador()
-    gerenciador.add_contato(nome="João", sobrenome="Silva", telefone="123456789", empresa="ABC LTDA", email="joao.silva@example.com")
-
+def test_save_contatos(mock_open, gerenciador_com_contatos):
     with patch("ProjetoAgenda.Agenda.gerenciador.Config.formatar_contato", return_value={"nome": "João", "sobrenome": "Silva"}):
         with patch("builtins.open", mock_open):
-            gerenciador.save_contatos()
+            gerenciador_com_contatos.save_contatos()
 
-    mock_open.assert_called_once_with(gerenciador.config.contatos_file, "w")
+    mock_open.assert_called_once_with(gerenciador_com_contatos.config.contatos_file, "w")
     mock_open.return_value.__enter__.return_value.write.assert_called_once()
