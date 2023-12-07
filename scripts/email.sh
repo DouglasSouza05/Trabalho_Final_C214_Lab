dc3b9d10a0dfb4456c18fbaf28976ac9e68ce49f
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-12-07 19:50:57 -0300

    Removendo comentarios do arquivo .sh e configurando o mailutils

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
-# Recupera o endereço de e-mail da variável de ambiente
 to_email="$EMAIL_VARIABLE"
 
 subject="Pipeline terminou"
 body="Pipeline do projeto de C214 terminou"
 
-# Use o comando mail para enviar o e-mail
+echo "set smtp-use-starttls" >> ~/.mailrc
+echo "set ssl-verify=ignore" >> ~/.mailrc
 echo "$body" | mail -s "$subject" "$to_email"
