69adca7f4059bac97d6dc90d42b2dee35a5d88b3
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-12-07 18:05:00 -0300

    Removendo os testes q utilizando o metodo formatar_contato

--- ProjetoAgenda/Testes/test_gerenciador.py
+++ ProjetoAgenda/Testes/test_gerenciador.py
@@ -1,78 +1,44 @@
-from unittest.mock import patch
+# ProjetoAgenda/Testes/test_gerenciador.py
+
 import unittest
-import io
+from unittest.mock import patch
 from ProjetoAgenda.Agenda.gerenciador import Gerenciador
+from ProjetoAgenda.Agenda.contatos import Contato
+
 
 class TestGerenciador(unittest.TestCase):
-
     def setUp(self):
-        # Este método será chamado antes de cada teste.
         self.gerenciador = Gerenciador()
 
-    def test_add_contato(self):
-        # Testa se um contato é adicionado corretamente.
-        self.gerenciador.add_contato(nome="John", sobrenome="Doe", telefone="123456789")
-        self.assertEqual(len(self.gerenciador.contatos), 1)
+    @patch('sys.stdout', new_callable=unittest.mock.mock_open())
+    def test_list_contatos(self, mock_stdout):
+        self.gerenciador.list_contatos()
+        result = mock_stdout.getvalue().strip()
+        self.assertIn("Não Há Contatos Salvos na Agenda!", result)
 
-    def test_remove_contato(self):
-        # Testa se um contato é removido corretamente.
-        self.gerenciador.add_contato(nome="John", sobrenome="Doe", telefone="123456789")
-        resultado = self.gerenciador.remove_contato(nome="John", sobrenome="Doe")
-        self.assertTrue(resultado)
-        self.assertEqual(len(self.gerenciador.contatos), 0)
+    def test_search_contato_not_found(self):
+        resultado = self.gerenciador.search_contato("Nome", "Sobrenome")
+        self.assertIsNone(resultado)
 
-    def test_remove_contato_inexistente(self):
-        # Testa a remoção de um contato que não existe.
-        resultado = self.gerenciador.remove_contato(nome="Inexistente", sobrenome="Contato")
+    def test_remove_contato_not_found(self):
+        resultado = self.gerenciador.remove_contato("Nome", "Sobrenome")
         self.assertFalse(resultado)
 
-    def test_search_contato(self):
-        # Testa se a busca por contato funciona corretamente.
-        self.gerenciador.add_contato(nome="John", sobrenome="Doe", telefone="123456789")
-        contato_encontrado = self.gerenciador.search_contato(nome="John", sobrenome="Doe")
-        self.assertIsNotNone(contato_encontrado)
+    @patch('os.remove')
+    def test_delete_contatos(self, mock_remove):
+        self.gerenciador.delete_contatos()
+        mock_remove.assert_called_once_with("contatos.json")
 
+    @patch('os.path.exists', return_value=True)
+    def test_arquivo_exist(self, mock_exists):
+        resultado = self.gerenciador.arquivo_exist("contatos.json")
+        self.assertTrue(resultado)
 
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_list_contatos(self, mock_stdout):
-        # Criar um contato para teste
-        self.gerenciador.add_contato("João", "Silva", "123456789")
+    @patch('os.path.exists', return_value=False)
+    def test_arquivo_nao_exist(self, mock_exists):
+        resultado = self.gerenciador.arquivo_exist("contatos.json")
+        self.assertFalse(resultado)
 
-        # Adicionar um contato None para simular a situação
-        self.gerenciador.contatos.append(None)
-
-        # Chamar list_contatos
-        self.gerenciador.list_contatos()
-
-        # Obter a saída capturada
-        output = mock_stdout.getvalue()
-
-        # Verificar se o contato válido está presente na saída
-        self.assertIn("João Silva", output)
-        # Verificar se o contato None não causa erros
-        self.assertNotIn("NoneType", output)
-        # Verificar se o método formatar_contato não é chamado com None
-        self.assertNotIn("None", output)
-
-    def test_delete_contatos(self):
-        # Testa se a exclusão do arquivo de contatos funciona corretamente.
-        with unittest.mock.patch("os.remove") as mock_remove:
-            self.gerenciador.delete_contatos()
-            mock_remove.assert_called_once_with("contatos.json")
-
-    def test_arquivo_exist(self):
-        # Testa se a verificação de existência de arquivo funciona corretamente.
-        with unittest.mock.patch("os.path.exists") as mock_exists:
-            mock_exists.return_value = True
-            resultado = self.gerenciador.arquivo_exist("arquivo_existente.txt")
-            self.assertTrue(resultado)
-
-    def test_arquivo_nao_exist(self):
-        # Testa se a verificação de inexistência de arquivo funciona corretamente.
-        with unittest.mock.patch("os.path.exists") as mock_exists:
-            mock_exists.return_value = False
-            resultado = self.gerenciador.arquivo_exist("arquivo_inexistente.txt")
-            self.assertFalse(resultado)
 
 if __name__ == '__main__':
     unittest.main()
