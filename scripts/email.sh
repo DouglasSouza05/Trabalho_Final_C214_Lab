bd7953c9328cb6540a94ba428b671e9291064933
LucasSResende <lucasresendesrs@gmail.com>
2023-12-09 15:23:26 -0300

    Notifications

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,12 +1,4 @@
-# Trabalho_Final_C214_Lab
 echo "Trabalho de C214 Lab"
-# Recupera o endereço de e-mail da variável de ambiente
-notify-send "Notificação em paralelo rodando" &
-
-# Executa o job
-./job.sh
-
-# Fim do trabalho
-echo "Testes finalizados"
 sudo apt-get install mailutils
+echo "Testes finalizados"
 echo "Enviando o e-mail do trabalho C214 Lab" | mail -s "Trabalho C214 Lab" ${EMAIL_LIST}
